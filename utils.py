4d6e7155b4a9e1a6403a8d74656049b4b58e333a
FreezeYourDiary <freezeyourdiary@gmail.com>
2025-05-14 16:07:04 +0200

    nowa funkcjonalnosc

--- utils.py
+++ utils.py
@@ -12,10 +12,23 @@ def subtract(a: int, b: int) -> int:
 
 
 def multiply(a: int, b: int) -> int:
-    """Return the product of a and b."""
+    """Return multiply of a and b."""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
-    """Return the quotient of a divided by b."""
+    """Return diff? of a divided by b."""
     return a / b
+
+
+def float_to_binary(x):
+    m = 5
+    n = 5
+    """convert float x, with base m .
+    """
+    if not (0 <= x <= 100):
+        raise ValueError("out of range range.")
+    x_scaled = round(x * 2 ** n)
+    return '{:0{}b}'.format(x_scaled, m + n)
+if __name__ == '__main__':
+    print(float_to_binary(5))
